968692ced1eca2ca42e98d441e9223c0ff83fdd5
osvaldo1123 <147023034+osvaldo1123@users.noreply.github.com>
2024-10-29 01:07:50 -0300

    Update SunlightSensor.js

--- SunlightSensor.js
+++ SunlightSensor.js
@@ -1,20 +1,22 @@
 class MAX44009 {
     constructor() {
+//Valores de mínimo e máximo de luminosidade (em lux) de um sensor modeo MAX4409 
       this.minLux = 0.045;
       this.maxLux = 188000;
       this.luxAtual = this.minLux;
     }
-  
+
+    // Metodo pra simular um valor de lux obtido pelo sensor
     sensorLux() {
       this.luxAtual = this.minLux + Math.random() * (this.maxLux - this.minLux);
       return this.luxAtual;
     }
   }
   
-  // Usage
 const virtualMAX44009 = new MAX44009();
 
+// Simula uma leitura do sensor a cada 2 segundos
 setInterval(() => {
     const data = virtualMAX44009.sensorLux();
-    console.log(`Simulated Lux Reading: ${data.toFixed(2)} lux`);
+    console.log(`Luminosidade: ${data.toFixed(2)} lux`);
   }, 2000);
